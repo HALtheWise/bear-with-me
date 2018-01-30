9bcc00fe6ff46dc4dcd7f053750623f01d482c26
jwb <jared.briskman@students.olin.edu>
2018-01-30 17:05:29 -0500

    Splits out app, models and db management

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
 
 @app.route('/')
 def hello_world():
