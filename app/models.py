518eabe89dede5384df92e67e7be6ae250536b81
Eric Miller <eric@legoaces.org>
2018-02-02 07:27:35 -0500

    Updates db with fields required for MVP

--- app/models.py
+++ app/models.py
@@ -2,10 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+    phone = db.Column(db.String(128))
+    active = db.Column(db.Boolean())
+    last_call = db.Column(db.DateTime())
 
     def __repr__(self):
         return '<User %r>' % self.name
-
